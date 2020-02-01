832038c2a262e9a5a8cd5f7892625f45bbc454dd
Silvia Chen <suiqingc@andrew.cmu.edu>
2020-02-01 11:09:52 -0500

    gamemanager script and checkpoint script

---
+++ Surroundings GGJ 2020/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class GameManagerScript : MonoBehaviour
+{
+    public static GameManagerScript GM = null; 
+    GameObject player; 
+
+    int playerLives;
+    float timeLeft;
+
+    public Vector3 respawnPoint = Vector3.zero;
+
+    private void Awake()
+    {
+        if (GM = null)
+        {
+            GM = this; 
+        }
+        else if (GM != this)
+        {
+            Destroy(this); 
+        }
+
+        DontDestroyOnLoad(this); 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("PlayerObject"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // kill player after some delay 
+    void KillPlayer(float delay)
+    {
+        playerLives--; 
+
+        if (playerLives <= 0)
+        {
+            // reload(reset) current scene 
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); 
+        }
+        else
+        {
+            Respawn(respawnPoint); 
+        }
+    }
+
+    // respawn at certain point 
+    void Respawn(Vector3 point)
+    {
+        player.transform.position = point; 
+    }
+}
