cf5d3f00f7aa18ba850f36ab9556abbd9dc7b5bc
Silvia Chen <suiqingc@andrew.cmu.edu>
2020-02-02 10:02:52 -0500

    created end text and fade in script

---
+++ Surroundings GGJ 2020/Assets/Scripts/FadeInEndScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class FadeInEndScript : MonoBehaviour
+{
+    public float showDelay = 1.5f;
+
+    public Text i;
+
+    private void Awake()
+    {
+        // start text as transparenti
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartFadeIn()); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator StartFadeIn()
+    {
+        yield return new WaitForSeconds(showDelay);
+
+        StartCoroutine(FadeTextToFullAlpha());
+
+    }
+
+    // adapted from https://forum.unity.com/threads/fading-in-out-gui-text-with-c-solved.380822/
+    public IEnumerator FadeTextToFullAlpha()
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        while (i.color.a < 1.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / displayTime));
+            yield return null;
+        }
+    }
+}
